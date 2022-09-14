35a59f66d0f1f8233d95142946775ffe22ce5bdf
persiopeo <persiopeo@hotmail.com>
2022-09-14 20:59:30 -0300

    fix: corrigindo erros no requisito-08 e 09

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,2 +1,2 @@
-db.produtos.deleteMany({ curtidas: { $lt: 50 } },
-  db.produtos.find({}, { nome: 1, _id: false }));
+db.produtos.deleteMany({ curtidas: { $lt: 50 } });
+  db.produtos.find({}, { nome: 1, _id: false });
