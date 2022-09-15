8e7018042db8bc9429876b00f2e868821e60a100
persiopeo <persiopeo@hotmail.com>
2022-09-15 11:54:51 -0300

    feat:

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,11 +1,12 @@
 db.produtos.updateMany(
-  { valoresNutricionais: {
-  $elemMatch: {
+  { valoresNutricionais: { $elemMatch: {
       tipo: "sódio",
       percentual: { $lt: 40, $gt: 20 },
   } } },
   { $push: { tags: "contém sódio" } },
   );
 
-db.produtos.find({}, 
-  { nome: 1, _id: false, tags: 1 });
+db.produtos.find(
+  {}, 
+  { nome: 1, _id: false, tags: 1 },
+);
