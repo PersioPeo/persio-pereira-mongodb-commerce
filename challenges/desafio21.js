f2507ec8409dffb1795b57b91e443c83df523b10
persiopeo <persiopeo@hotmail.com>
2022-09-15 11:08:36 -0300

    fix: corrindo erros de digitação

--- challenges/desafio21.js
+++ challenges/desafio21.js
@@ -1,11 +1,13 @@
-db.produtos.One(
+db.produtos.updateOne(
   { nome: "Cheddar McMelt" },
   { $pop: { ingredientes: 1 } },
 );
 
-db.produtos.find({},
+db.produtos.find(
+  {},
 { 
   nome: true,
   _id: false,
   ingredientes: 1,
-});
+},
+);
